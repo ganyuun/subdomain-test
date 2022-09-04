5135c98769881b99ed5be8838790c089e155b3d1
ganyuun <kenkanekifromikea@gmail.com>
2022-09-04 12:56:44 -0500

    remove tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,3 @@
 import { defineConfig } from 'astro/config';
 
-// dark mode
-import tailwind from "@astrojs/tailwind";
-import preact from "@astrojs/preact";
-
-export default defineConfig({
-  integrations: [preact(), tailwind()],
-  site: 'https://vastaya.ganyuun.com',
-});
+export default defineConfig({});
