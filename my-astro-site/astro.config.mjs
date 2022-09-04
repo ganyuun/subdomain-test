04a7fb5589ae45641e8baf1d1116130920a77b39
ganyuun <kenkanekifromikea@gmail.com>
2022-09-04 03:12:15 -0500

    deploy instructions, move CNAME to public folder

--- my-astro-site/astro.config.mjs
+++ my-astro-site/astro.config.mjs
@@ -4,8 +4,9 @@ import { defineConfig } from 'astro/config';
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   integrations: [preact(), tailwind()],
+  site: 'https://vastaya.ganyuun.com',
 });
-
